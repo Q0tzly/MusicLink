225031ba4fda3eee9e4a4890d35d30bdc9589b84
Q0tzly <g9164902@gmail.com>
2024-01-15 00:06:58 +0900

    add: added 'src/lib.rs', 'src/request.rs'.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
-    let url = "https://www.youtube.com/watch?v=W13Ydr_AcjI";
+        
+        let url = "https://www.youtube.com/watch?v=W13Ydr_AcjI";
 
-    let resp = reqwest::get(url)
-        .await?
-        .text()
-        .await?;
-    println!("{:#?}", resp);
-    Ok(())
+            let resp = reqwest::get(url)
+                        .await?
+                                .text()
+                                        .await?;
+        println!("{:#?}", resp);
+            Ok(())
 }
