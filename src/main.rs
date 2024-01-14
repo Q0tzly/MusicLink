d9e28ed3ff897a2f2b18fe193d96a1069f986ff9
Q0tzly <g9164902@gmail.com>
2024-01-14 23:01:08 +0900

    test: can get body in url with using reqwest.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    let url = "https://www.youtube.com/watch?v=W13Ydr_AcjI";
+
+    let resp = reqwest::get(url)
+        .await?
+        .text()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
